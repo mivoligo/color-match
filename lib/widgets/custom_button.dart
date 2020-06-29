6f5791e0e1a9e073c71d66dc6e51a8ec96e49047
Michal Predotka <mivoligo@gmail.com>
2020-06-29 16:48:09 +0100

    Edit CustomButton widget

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
+
+  final Color buttonColor;
+  final VoidCallback buttonOnTap;
+  final IconData buttonIcon;
+
+  CustomButton({
+    Key key,
+    this.buttonColor,
+    this.buttonOnTap,
+    this.buttonIcon,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      shape: CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      color: buttonColor,
+      child: InkWell(
+        splashColor: Colors.white54,
+        onTap: buttonOnTap,
+        child: SizedBox(
+          width: 36,
+          height: 36,
+          child: Icon(
+            buttonIcon,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
